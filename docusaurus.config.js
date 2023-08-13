201d2f0a9462e283b0655c06d637a46e1e37bb94
mhanifmuhsin <m.hanif.muhsin@gmail.com>
2023-08-13 10:02:27 +0700

    fix: :bug: config deploymentBranch

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,6 +20,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: "mhanifmuhsin", // Usually your GitHub org/user name.
   projectName: "mhanifmuhsin.github.io", // Usually your repo name.
+  deploymentBranch: "gh-pages",
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
