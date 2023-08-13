a15a832af8017fef37b95ba932a7166d2edf23df
mhanifmuhsin <m.hanif.muhsin@gmail.com>
2023-08-13 09:49:12 +0700

    feat: :wrench: update configuration gh-pages

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,19 +7,19 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: "My Doc",
-  tagline: "Dinosaurs are cool",
+  tagline: "Belajar bersama, sama-sama belajar",
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://your-docusaurus-test-site.com",
+  url: "https://mhanifmuhsin.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "facebook", // Usually your GitHub org/user name.
-  projectName: "docusaurus", // Usually your repo name.
+  organizationName: "mhanifmuhsin", // Usually your GitHub org/user name.
+  projectName: "mhanifmuhsin.github.io", // Usually your repo name.
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
