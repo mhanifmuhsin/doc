f8d9d1dabeca74948586fc475c5e66d212eb25d9
mhanifmuhsin <m.hanif.muhsin@gmail.com>
2023-08-13 09:30:46 +0700

    feat: :hammer: auto collapse categories

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -62,6 +62,11 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
+      docs: {
+        sidebar: {
+          autoCollapseCategories: true,
+        },
+      },
       image: "img/docusaurus-social-card.jpg",
       navbar: {
         title: "mhanifmuhsin",
